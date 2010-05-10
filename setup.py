ad08889c9b3561cac57ab8cab2d14c5e40280971
Oliver Beckstein <oliver.beckstein@bioch.ox.ac.uk>
2010-05-10 10:56:37 +0100

    mdpow-rebuild-fep: script to recreate the fep pickle file - needed in order to either change paths - or when the pickle file got destroyed (e.g. when I changed some code...)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ octanol. It uses Gromacs (http://www.gromacs.org) for the molecular dynamics
       download_url="http://sbcb.bioch.ox.ac.uk/oliver/download/Python/",
       keywords="science Gromacs analysis 'molecular dynamics'",
       packages=find_packages(exclude=['examples']),
-      scripts = ['scripts/mdpow-pow', 'scripts/mdpow-check'],
+      scripts = ['scripts/mdpow-pow', 'scripts/mdpow-check', 'scripts/mdpow-rebuild-fep'],
       package_data={'mdpow': ['top/*', 'templates/*'], },
       install_requires = ['numpy>=1.0', 'scipy',
                           'GromacsWrapper>=0.1.10'],
