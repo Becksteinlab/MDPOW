cda05ad38c9a983b3f8bc7d5e00607d6e125a4ae
Oliver Beckstein <orbeckst@gmail.com>
2011-07-27 18:38:07 -0400

    benzene example: more docs and improved session.py

--- example/benzene/session.py
+++ example/benzene/session.py
@@ -3,14 +3,20 @@ S = mdpow.equil.WaterSimulation(molecule="BNZ")
 S.topology("benzene.itp")
 S.solvate(struct="benzene.pdb")
 S.energy_minimize()
-S.MD_relaxed(runtime=0.5)
+S.MD_relaxed(runtime=5)   # should be at least 1e3 ps for production not just 5 ps
 
-# run simulation externally or use
-# gromacs.run.mdrun() ...
+# run simulation externally or use MDrunner
+# (see docs for using mpi etc)
+import gromacs
+r = gromacs.run.MDrunner(dirname=S.dirs['MD_relaxed'], deffnm="md", c="md.pdb", cpi=True, append=True, v=True)
+r.run()   # runs mdrun in the python shell
 
-S.MD(runtime=5, qscript=['local.sh'])
 
+S.MD(runtime=10, qscript=['local.sh'])  # should be at least 10e3 ps for production, not just 10 ps
 # run simulation
+r = gromacs.run.MDrunner(dirname=S.dirs['MD_NPT'], deffnm="md", c="md.pdb", cpi=True, append=True, v=True)
+r.run()   # runs mdrun in the python shell
+
 
 import mdpow.fep
 gwat = mdpow.fep.Ghyd(simulation=S, runtime=10)
