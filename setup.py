e7177ff129a25f6b90b5ffdb013cef21435c3f6f
Ian Kenney <ianmichaelkenney@gmail.com>
2014-09-22 15:46:51 -0700

    all dependencies on pypi

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ octanol. It uses Gromacs (http://www.gromacs.org) for the molecular dynamics
       install_requires = ['numpy>=1.0', 'scipy',
                           'GromacsWrapper>0.2.5',
                           'RecSQL>=0.7.5'],
-      dependency_links = ["http://sbcb.bioch.ox.ac.uk/oliver/download/Python/"],
       zip_safe = True,
 )
 
