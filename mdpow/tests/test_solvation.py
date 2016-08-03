32073aa963079e7487ad640298f7b9b8bd91b2be
Ian Kenney <ian.kenney@asu.edu>
2016-08-03 13:30:43 -0700

    implemented solvation testing

--- mdpow/tests/test_solvation.py
+++ mdpow/tests/test_solvation.py
@@ -1,29 +1,37 @@
 import mdpow.equil
 import tempdir as td
 import os
+import shutil
 
 class TestSolvation(object):
     sim_filename = ".simulation"
     sims = {"water" : mdpow.equil.WaterSimulation,
             "octanol" : mdpow.equil.OctanolSimulation,
-            "cyclohexane" : mdpow.equil.CyclohexaneSimulation}
+            "cyclohexane" : mdpow.equil.CyclohexaneSimulation,
+           }
 
     def setup(self):
         self.tmpdir = td.TempDir()
         self.old_path = os.getcwd()
-        print(self.tmpdir.name)
-        print(self.old_path)
-        assert 0
+        self.resources = self.old_path + "/mdpow/tests/testing_resources"
+        self.solvation_paths = self.resources + "/stages/solvation/water/benzene"
+        [shutil.copy('{0}/molecules/benzene/{1}'.format(self.resources, x), self.tmpdir.name) for x in ['benzene.pdb','benzene.itp']]
 
-    def _test_solvation(structure,solvent):
-        raise NotImplementedError
+    def _test_solvation(self, solvent):
+        os.chdir(self.tmpdir.name)
+        try:
+            S = self.sims[solvent](molecule='BNZ')
+            S.topology(itp='benzene.itp')
+            S.solvate(struct='benzene.pdb')
+            assert 1
+        finally:
+            os.chdir(self.old_path)
 
-    def test_solvation_water(structure):
-        return _test_solvation(structure,'water')
+    def test_solvation_water(self):
+        return self._test_solvation('water')
 
-    def test_solvation_octanol(structure):
-        return _test_solvation(structure,'octanol')
+    def test_solvation_octanol(self):
+        return self._test_solvation('octanol')
 
-    def test_solvation_cyclohexane(structure):
-        return _test_solvation(structure,'cyclohexane')
-
+    def test_solvation_cyclohexane(self):
+        return self._test_solvation('cyclohexane')
