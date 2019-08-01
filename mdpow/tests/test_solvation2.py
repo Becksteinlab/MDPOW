a6540408882526e9faa72d062fb85e8cc46dd16d
VOD555 <gn8008@hotmail.com>
2019-08-01 01:01:07 -0700

    mark wetoctanol test with xfail for gromacs 2019

--- mdpow/tests/test_solvation2.py
+++ mdpow/tests/test_solvation2.py
@@ -3,6 +3,7 @@ import shutil
 
 import mdpow.equil
 from gromacs.utilities import in_dir
+import gromacs
 
 import pytest
 
@@ -18,24 +19,29 @@ def setup(tmpdir):
         shutil.copy(orig, newdir.dirname)
     return newdir.dirname
 
+def solvation(setup, solvent):
     sims = {"water" : mdpow.equil.WaterSimulation,
             "octanol" : mdpow.equil.OctanolSimulation,
             "cyclohexane" : mdpow.equil.CyclohexaneSimulation,
             "wetoctanol" : mdpow.equil.WetOctanolSimulation,
-    }
-
-
-@pytest.mark.parametrize("sim", [
-mdpow.equil.WaterSimulation,
-mdpow.equil.OctanolSimulation,
-mdpow.equil.CyclohexaneSimulation,
-mdpow.equil.WetOctanolSimulation
-])
-def test_solvation(setup, sim):
+            }
     with in_dir(setup, create=False):
         try:
-            S = sim(molecule='BNZ')
+            S = sims[solvent](molecule='BNZ')
             S.topology(itp='benzene.itp')
             S.solvate(struct='benzene.pdb')
         except Exception:
             raise AssertionError('Solvation failed.')
+
+def test_solvation_water(setup):
+    solvation(setup, "water")
+
+def test_solvation_octanol(setup):
+    solvation(setup, "octanol")
+
+def test_solvation_cyclohexane(setup):
+    solvation(setup, "cyclohexane")
+
+@pytest.mark.xfail("gromacs.release().startswith('2019')")
+def test_solvation_wetoctanol(setup):
+    solvation(setup, "wetoctanol")
