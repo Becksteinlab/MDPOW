e1b35b90d420102334558ce4505891a5f75d3781
Shujie Fan <gn8008@hotmail.com>
2021-03-23 18:39:44 -0700

    Allow mdpow analysis scripts to select alchemlyb estimators (#134)
    * add options to p_transfer, collect_alchemlyb and analyze_alchemlyb
    
    * add options in mdpow-pow
    
    * don't generate plot when estimator is alchemlyb
    
    * add error check for estimator
    
    * Do not concatenate edr files if there is only 1 edr file

--- mdpow/fep.py
+++ mdpow/fep.py
@@ -773,9 +773,12 @@ class Gsolv(Journalled):
             tpr_files = [self.dgdl_tpr(self.wdir(component, l)) for l in lambdas]
             for tpr, edr in zip(tpr_files, edr_files):
                 dirct = os.path.abspath(os.path.dirname(tpr))
-                total_edr = self.dgdl_total_edr(dirct)
-                logger.info("  {0} --> {1}".format('edrs', total_edr))
-                gromacs.eneconv(f=edr, o=total_edr)
+                if len(edr) == 1:
+                    total_edr = edr[0]
+                else:
+                    total_edr = self.dgdl_total_edr(dirct)
+                    logger.info("  {0} --> {1}".format('edrs', total_edr))
+                    gromacs.eneconv(f=edr, o=total_edr)
                 xvgfile = os.path.join(dirct, self.deffnm + ".xvg")  # hack
                 logger.info("  {0} --> {1}".format(total_edr, xvgfile))
                 gromacs.g_energy(s=tpr, f=total_edr, odh=xvgfile)
@@ -1003,7 +1006,7 @@ class Gsolv(Journalled):
         self.logger_DeltaA0()
         return self.results.DeltaA.Gibbs
 
-    def collect_alchemlyb(self, start=0, stop=None, stride=None, autosave=True, autocompress=True):
+    def collect_alchemlyb(self, SI=False, start=0, stop=None, stride=None, autosave=True, autocompress=True):
         extract = self.estimators[self.method]['extract']
 
         if autocompress:
@@ -1017,7 +1020,7 @@ class Gsolv(Journalled):
             for l in lambdas:
                 xvg_file = self.dgdl_xvg(self.wdir(component, l))
                 xvg_df = extract(xvg_file, T=self.Temperature).iloc[start:stop:stride]
-                if self.SI:
+                if (self.SI or SI):
                     ts = _extract_dataframe(xvg_file).iloc[start:stop:stride]
                     ts = pd.DataFrame({'time': ts.iloc[:,0], 'dhdl': ts.iloc[:,1]})
                     ts = ts.set_index('time')
@@ -1028,7 +1031,7 @@ class Gsolv(Journalled):
         if autosave:
             self.save()
 
-    def analyze_alchemlyb(self, start=0, stop=None, stride=None, force=False, autosave=True):
+    def analyze_alchemlyb(self, SI=False, start=0, stop=None, stride=None, force=False, autosave=True):
         stride = stride or self.stride
         start = start or self.start
         stop = stop or self.stop
@@ -1042,11 +1045,11 @@ class Gsolv(Journalled):
 
         if force or not self.has_dVdl():
             try:
-                self.collect_alchemlyb(start, stop, stride, autosave=False)
+                self.collect_alchemlyb(SI, start, stop, stride, autosave=False)
             except IOError as err:
                 if err.errno == errno.ENOENT:
                     self.convert_edr()
-                    self.collect_alchemlyb(start, stop, stride, autosave=False)
+                    self.collect_alchemlyb(SI, start, stop, stride, autosave=False)
                 else:
                     logger.exception()
                     raise
@@ -1309,12 +1312,29 @@ def p_transfer(G1, G2, **kwargs):
            force rereading of data files even if some data were already stored [False]
        *stride*
            analyze every *stride*-th datapoint in the dV/dlambda files
+       *start*
+           Start frame of data analyzed in every fep window.
+       *stop*
+           Stop frame of data analyzed in every fep window.
+       *SI*
+           Set to ``True`` if you want to perform statistical inefficiency
+           to preprocess the data.
+       *estimator*
+           Set to ``alchemlyb`` if you want to use alchemlyb estimators,
+           or ``mdpow`` if you want the default TI method.
+       *method*
+           Use `TI`, `BAR` or `MBAR` method in `alchemlyb`, or `TI` in `mdpow`.
+
     :Returns: (transfer free energy, log10 of the water-octanol partition coefficient = log Pow)
 
     """
 
     kwargs.setdefault('force', False)
-
+    estimator = kwargs.pop('estimator', 'mdpow')
+    if not estimator in ('mdpow', 'alchemlyb'):
+        errmsg = "estimator = %r is not supported, must be 'mdpow' or 'alchemlyb'" % estimator
+        logger.error(errmsg)
+        raise ValueError(errmsg)
     if G1.molecule != G2.molecule:
         raise ValueError("The two simulations were done for different molecules.")
     if G1.Temperature != G2.Temperature:
@@ -1323,15 +1343,35 @@ def p_transfer(G1, G2, **kwargs):
     logger.info("[%s] transfer free energy %s --> %s calculation",
                 G1.molecule, G1.solvent_type, G2.solvent_type)
     for G in (G1, G2):
+        # for fep files generated with old code which doesn't have these attributes
+        if not hasattr(G, 'start'):
+            G.start = kwargs.pop('start', 0)
+        if not hasattr(G, 'stop'):
+            G.stop = kwargs.pop('stop', None)
+        if not hasattr(G, 'SI'):
+            G.SI = kwargs.pop('SI', False)
+
+        # for this version. use the method given instead of the one in the input cfg file
+        G.method = kwargs.pop('method', 'TI')
         if kwargs['force']:
-            G.analyze(**kwargs)
+            if estimator == 'mdpow':
+                G.analyze(**kwargs)
+                if G.method != "TI":
+                    errmsg = "Method %s is not implemented in MDPOW, use estimator='alchemlyb'" % G.method
+                    logger.error(errmsg)
+                    raise ValueError(errmsg)
+            elif estimator == 'alchemlyb':
+                G.analyze_alchemlyb(**kwargs)
+
         try:
             G.results.DeltaA.Gibbs
             G.logger_DeltaA0()
         except (KeyError, AttributeError):   # KeyError because results is a AttributeDict
             logger.warn("Must analyze simulation because no hydration free energy data available...")
-            G.analyze(**kwargs)
-
+            if estimator == 'mdpow':
+                G.analyze(**kwargs)
+            elif estimator == 'alchemlyb':
+                G.analyze_alchemlyb(**kwargs)
     # x.Gibbs are QuantityWithError so they do error propagation
     transferFE = G2.results.DeltaA.Gibbs - G1.results.DeltaA.Gibbs
     # note minus sign, with our convention of the free energy difference to be
@@ -1375,6 +1415,18 @@ def pOW(G1, G2, **kwargs):
            force rereading of data files even if some data were already stored [False]
        *stride*
            analyze every *stride*-th datapoint in the dV/dlambda files
+       *start*
+           Start frame of data analyzed in every fep window.
+       *stop*
+           Stop frame of data analyzed in every fep window.
+       *SI*
+           Set to ``True`` if you want to perform statistical inefficiency
+           to preprocess the data.
+       *estimator*
+           Set to ``alchemlyb`` if you want to use alchemlyb estimators,
+           or ``mdpow`` if you want the default TI method.
+       *method*
+           Use `TI`, `BAR` or `MBAR` method in `alchemlyb`, or `TI` in `mdpow`.
 
     :Returns: (transfer free energy, log10 of the octanol/water partition coefficient = log Pow)
     """
@@ -1408,6 +1460,18 @@ def pCW(G1, G2, **kwargs):
            force rereading of data files even if some data were already stored [False]
        *stride*
            analyze every *stride*-th datapoint in the dV/dlambda files
+       *start*
+           Start frame of data analyzed in every fep window.
+       *stop*
+           Stop frame of data analyzed in every fep window.
+       *SI*
+           Set to ``True`` if you want to perform statistical inefficiency
+           to preprocess the data.
+       *estimator*
+           Set to ``alchemlyb`` if you want to use alchemlyb estimators,
+           or ``mdpow`` if you want the default TI method.
+       *method*
+           Use `TI`, `BAR` or `MBAR` method in `alchemlyb`, or `TI` in `mdpow`.
 
     :Returns: (transfer free energy, log10 of the cyclohexane/water partition coefficient = log Pcw)
     """
