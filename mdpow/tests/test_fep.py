2797861eb73d87bb571169314a70f3b6c1d71022
alescoulie <alia.lescoulie@gmail.com>
2021-07-27 11:15:15 -0700

    Cleanup test_fep.py

--- mdpow/tests/test_fep.py
+++ mdpow/tests/test_fep.py
@@ -66,6 +66,7 @@ class TestFEPschedule(object):
     def test_Coulomb(self):
         return self._test_schedule('Coulomb')
 
+<<<<<<< HEAD
     @pytest.mark.parametrize('component', ['VDW', 'Coulomb'])
     def test_copy(self, component):
         section = 'FEP_schedule_{0}'.format(component)
@@ -99,6 +100,13 @@ class TestFEPschedule(object):
 
     def test_iterable(self):
         assert not mdpow.config.iterable('test')
+=======
+    def test_gromacs_import(self, monkeypatch):
+        monkeypatch.setitem(sys.modules, 'gromacs.setup', None)
+        with pytest.raises(ImportError):
+            reload_module(gromacs.setup)
+
+>>>>>>> Cleanup test_fep.py
 
     def _test_schedule(self, component):
         section = 'FEP_schedule_{0}'.format(component)
