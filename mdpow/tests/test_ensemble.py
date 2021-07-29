0b4bac2f85c2c1860d15c06e20dade1042a58ecf
Alia Lescoulie <72271618+ALescoulie@users.noreply.github.com>
2021-07-29 12:08:27 -0700

    Commit tests for Ensemble object in analysis.py, adjust test matrix

--- mdpow/tests/test_ensemble.py
+++ mdpow/tests/test_ensemble.py
@@ -1,6 +1,4 @@
-from __future__ import absolute_import
-
-from . import tempdir
+import mdpow.tests.tempdir as td
 
 import os.path
 import sys
@@ -15,6 +13,7 @@ from six.moves import cPickle as pickle
 
 from mdpow.analysis import Ensemble
 import mdpow.fep
+from gromacs.utilities import in_dir
 
 from pkg_resources import resource_filename
 RESOURCES = py.path.local(resource_filename(__name__, 'testing_resources'))
@@ -25,6 +24,7 @@ ensemble_keys = [('water', 'Coulomb', '0000'),
                  ('water', 'Coulomb', '0500'),
                  ('water', 'Coulomb', '0750'),
                  ('water', 'Coulomb', '1000'),
+                 ('water', 'VDW', '0000'),
                  ('water', 'VDW', '0050'),
                  ('water', 'VDW', '0100'),
                  ('water', 'VDW', '0200'),
@@ -33,6 +33,7 @@ ensemble_keys = [('water', 'Coulomb', '0000'),
                  ('water', 'VDW', '0500'),
                  ('water', 'VDW', '0600'),
                  ('water', 'VDW', '0650'),
+                 ('water', 'VDW', '0700'),
                  ('water', 'VDW', '0750'),
                  ('water', 'VDW', '0800'),
                  ('water', 'VDW', '0850'),
@@ -41,67 +42,41 @@ ensemble_keys = [('water', 'Coulomb', '0000'),
                  ('water', 'VDW', '1000')]
 
 
-def fix_manifest(topdir):
-    """Create a temporary manifest with a custom `path`.
+class TestEnsemble(object):
+    def setup(self):
+        self.tmpdir = td.TempDir()
+        self.old_path = os.getcwd()
+        self.resources = os.path.join(
+            self.old_path, 'mdpow', 'tests', 'testing_resources')
+        m = pybol.Manifest(os.path.join(self.resources, 'manifest.yml'))
+        m.assemble('FEP', self.tmpdir.name)
 
-    Fix manifest in a local temporary copy in existing dir topdir
-    where the `path` is an absolute path to our "states" directory. We
-    use `pkg_resources.resource_filename` to anchor the path.
-
-    Arguments
-    ---------
-    topdir : py.path.local
-        existing temporary directory (as provided by, for instance,
-        `pytest.tmpdir`)
-
-    Returns
-    -------
-    new_manifest : py.path.local
-        Path to the new manifest.
-
-    Example
-    -------
-    Use as ::
-
-        new_manifest = fix_manifest(tmpdir)
-        m = pybol.Manifest(new_manifest.strpath)
-
-    """
-    manifest = yaml.safe_load(MANIFEST.open())
-    # simple heuristic: last element of the recorded manifest::path is the name
-    # of the states directory, typically 'states' (from .../testing_resources/states)
-    manifest['path'] = RESOURCES.join(os.path.basename(manifest['path'])).strpath
-    new_manifest = topdir.join("local_manifest.yml")
-    yaml.dump(manifest, stream=new_manifest.open("w"))
-    return new_manifest
-
-
-# session scope if read-only use
-
-@pytest.fixture(scope="function")
-def fep_benzene_directory(tmpdir_factory):
-    topdir = tmpdir_factory.mktemp('ensemble')
-    m = pybol.Manifest(fix_manifest(topdir).strpath)
-    m.assemble('FEP', topdir.strpath)
-    return topdir.join("benzene")
-
-
-class TestAnalyze(object):
-    def get_Gsolv(self, pth):
-        gsolv = pth.join("FEP", "water", "Gsolv.fep")
-        # Needed to load old pickle files in python 3
-        if sys.version_info.major >= 3:
-            with open(gsolv, 'rb') as f:
-                G = pickle.load(f, encoding='latin1')
-                # patch paths
-        elif sys.version_info.major == 2:
-            G = pickle.load(gsolv.open())
-        G.basedir = pth.strpath
-        G.filename = gsolv.strpath
-        return G
+    def teardown(self):
+        self.tmpdir.dissolve()
 
     def test_build_ensemble(self):
-        G = self.get_Gsolv(fep_benzene_directory)
-        G.convert_edr()
-        bnz = Ensemble(dirname=fep_benzene_directory, solvents=['water'])
-        assert bnz.get_keys() == ensemble_keys
+        with in_dir(os.path.join(self.resources, 'states'), create=False):
+            bnz = Ensemble(dirname='benzene', solvents=['water'])
+            diff = set(bnz.get_keys()) ^ set(ensemble_keys)
+            assert not diff
+
+    def test_add_remove_systems(self):
+        with in_dir(os.path.join(self.resources, 'states'), create=False):
+            bnz = Ensemble()
+            l_dir = os.path.join(os.curdir, 'benzene', 'FEP', 'water', 'VDW', '0000')
+            top_dir = os.path.join(l_dir, 'md.gro')
+            trj_dir = os.path.join(l_dir, 'md.xtc')
+            bnz.add_system(('water', 'VDW', '0000'), topology=top_dir, trajectory=trj_dir)
+            assert bnz.get_keys() == [('water', 'VDW', '0000')]
+            assert bnz.num_systems == 1
+            assert len(bnz) == 1
+            bnz.pop_system(('water', 'VDW', '0000'))
+            assert bnz.num_systems == 0
+            assert len(bnz) == 0
+
+    def test_selection(self):
+        with in_dir(os.path.join(self.resources, 'states'), create=False):
+            bnz = Ensemble(dirname='benzene', solvents=['water'])
+            solute = bnz.select_atoms('not resname SOL')
+            for k in solute.keys():
+                assert len(solute[k]) == 12
