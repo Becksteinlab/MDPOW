4134d774a969207969033099911a898ada2ef4f4
Alia Lescoulie <72271618+ALescoulie@users.noreply.github.com>
2021-07-29 12:07:17 -0700

    Commit analysis.py

--- mdpow/analysis.py
+++ mdpow/analysis.py
@@ -1,5 +1,5 @@
 # mdpow: analysis.py
-# 2021 Alia Lesocoulie
+# 2021 Alia Lescoulie
 
 """
 
@@ -7,12 +7,11 @@
 from __future__ import absolute_import
 
 import os
-import glob
 
 import gromacs
 from gromacs.utilities import in_dir
 import MDAnalysis as mda
-from MDAnalysis.exceptions import FileFormatWarning, NoDataError, MissingDataWarning
+from MDAnalysis.exceptions import FileFormatWarning, NoDataError, MissingDataWarning, SelectionError
 import numpy
 import pandas as pd
 
@@ -20,8 +19,6 @@ import six
 
 import logging
 logger = logging.getLogger('mdpow.analysis')
-if not hasattr(logger, 'warning'):
-    logger.warning = logger.warn()
 
 
 class NoDataWarning(Exception):
@@ -51,16 +48,18 @@ class Ensemble(object):
         self.system_keys = []
         self.solvents = solvents
 
+        if dirname is None:
+            return
         if not os.path.exists(dirname):
             logger.error("Directory %s does not exist" % dirname)
         self.ensemble_dir = dirname
         self.build_ensemble()
 
     def __repr__(self):
-        print("<Ensemble Containing %s Universes>", self.num_systems)
+        return "<Ensemble Containing %s Universes>", self.num_systems
 
     def __len__(self):
-        return  self.num_systems
+        return self.num_systems
 
     def __getitem__(self, index):
         """Allows dictionary like indexing"""
@@ -72,7 +71,10 @@ class Ensemble(object):
 
     def __iter__(self):
         """Returns list of systems with object iterated over"""
-        return [system for system in self[self.system_keys]]
+        sys_list = []
+        for key in self.system_keys:
+            sys_list.append(self[key])
+        return sys_list
 
     @staticmethod
     def _load_dir_unv():
@@ -82,12 +84,12 @@ class Ensemble(object):
         more than one trajectory attempts to load both of them
         in a universe if that fail will try to load each individually"""
         cur_dir = os.listdir(os.curdir)
-        top = []
         trj = []
+        top = []
         for file in cur_dir:
-            if '.xvg' in file:
+            if file.endswith('.xtc'):
                 trj.append(file)
-            if '.gro' or '.pdb' in file:
+            if file.endswith('.gro'):
                 top.append(file)
         if len(top) > 1:
             logger.warning('More than one topology detected in %s will just use first topology' % os.curdir)
@@ -98,14 +100,14 @@ class Ensemble(object):
         try:
             system = mda.Universe(top[0], trj)
             return system
-        except FileFormatWarning or MissingDataWarning or NoDataError:
+        except FileFormatWarning or MissingDataWarning or NoDataError or ValueError:
             logger.warning('Mutiple trajectories deteceted in %s attempting'
                            ' to find correct one for topology' % os.curdir)
             while len(trj) > 0:
                 try:
                     system = mda.Universe(top[0], top.pop())
                     return system
-                except FileFormatWarning or MissingDataWarning or NoDataError:
+                except FileFormatWarning or MissingDataWarning or NoDataError or ValueError:
                     continue
             logger.warning('No files in %s are compatible skipping directory' % os.curdir)
             raise NoDataWarning
@@ -114,7 +116,7 @@ class Ensemble(object):
         return self.system_keys
 
     def build_ensemble(self):
-        """Goes through given mdpow directory and attempts to build univserse in the lambda directories."""
+        """Goes through given mdpow directory and attempts to build universe in the lambda directories."""
         fep_dir = os.path.join(self.ensemble_dir, 'FEP')
         for solvent in self.solvents:  # Ugly set of loops, may have to find way to clean up
             solvent_dict = {'Coulomb': {},
@@ -128,14 +130,39 @@ class Ensemble(object):
                         files = os.listdir(os.curdir)
                         for file in files:
                             if os.path.isdir(file):
-                                with in_dir(file, create=True):
+                                with in_dir(file, create=False):
                                     try:
-                                        solvent_dict[dirs, file] = self._load_dir_unv()
+                                        self.ensemble[solvent, dirs, file] = self._load_dir_unv()
                                         self.system_keys.append((solvent, dirs, file))
+                                        self.num_systems += 1
                                     except NoDataWarning:
                                         continue
-                            else:
-                                pass
-                else:
-                    pass
-            self.ensemble[solvent] = solvent_dict
+
+    def add_system(self, key, topology=None, trajectory=None):
+        if not os.path.exists(trajectory) and os.path.exists(topology):
+            logger.error("%s does not exist" % trajectory)
+        self.ensemble[key] = mda.Universe(topology, trajectory)
+        self.system_keys.append(key)
+        self.num_systems += 1
+
+    def pop_system(self, key):
+        try:
+            system = self.ensemble.pop(key)
+            self.num_systems -= 1
+            return system
+        except KeyError as err:
+            logger.error("%s with key %r" % (err, key))
+
+    def select_atoms(self, selection):
+        """Returns atom groups for Universe from objects
+
+        Presumes that solute in systems is the same"""
+        selections = {}
+        for key in self.system_keys:
+            try:
+                ag = self[key].select_atoms(selection)
+                selections[key] = ag
+            except SelectionError as err:
+                logger.warning("%s on system %r" % (err, key))
+                continue
+        return selections
