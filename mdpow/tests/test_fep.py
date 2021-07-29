070f37b5a058c4579d7bb5f1386a0b6bc2f38e56
Alia Lescoulie <72271618+ALescoulie@users.noreply.github.com>
2021-07-29 15:04:40 -0700

    add deepcopy test in test_fep.py

--- mdpow/tests/test_fep.py
+++ mdpow/tests/test_fep.py
@@ -8,6 +8,7 @@ import pytest
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_almost_equal
 from scipy import constants
+from copy import deepcopy
 
 import mdpow
 import mdpow.fep
@@ -64,6 +65,26 @@ class TestFEPschedule(object):
 
     def test_Coulomb(self):
         return self._test_schedule('Coulomb')
+
+    @pytest.mark.parametrize('component', ['VDW', 'Coulomb'])
+    def test_copy(self, component):
+        section = 'FEP_schedule_{0}'.format(component)
+        schedule = deepcopy(mdpow.fep.FEPschedule.load(self.cfg, section))
+        reference = self.reference[component]
+
+        for k in schedule:
+            assert k in reference, "additional entry {0} in runinput.yml".format(k)
+
+        for k in reference:
+            assert k in schedule, "missing entry {0} in runinput.yml".format(k)
+
+        for k in schedule.keys():
+            if k == "lambdas":
+                assert_array_almost_equal(schedule[k], reference[k],
+                                          err_msg="FEP schedule {0} mismatch".format(k))
+            else:
+                assert schedule[k] == reference[k], \
+                    "mismatch between loaded FEP schedule entry {0} and reference".format(k)
 
     def _test_schedule(self, component):
         section = 'FEP_schedule_{0}'.format(component)
