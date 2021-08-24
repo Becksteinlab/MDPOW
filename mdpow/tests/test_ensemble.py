0f87f6768177f5a4139314f05ed9be884a9a58d4
ALescoulie <alescoul@calpoly.edu>
2021-08-23 17:15:18 -0700

    Replace testfiles

--- mdpow/tests/test_ensemble.py
+++ mdpow/tests/test_ensemble.py
@@ -22,25 +22,11 @@ RESOURCES = py.path.local(resource_filename(__name__, 'testing_resources'))
 MANIFEST = RESOURCES.join("manifest.yml")
 
 ensemble_keys = [('water', 'Coulomb', '0000'),
-                 ('water', 'Coulomb', '0250'),
                  ('water', 'Coulomb', '0500'),
-                 ('water', 'Coulomb', '0750'),
                  ('water', 'Coulomb', '1000'),
                  ('water', 'VDW', '0000'),
-                 ('water', 'VDW', '0050'),
-                 ('water', 'VDW', '0100'),
-                 ('water', 'VDW', '0200'),
-                 ('water', 'VDW', '0300'),
-                 ('water', 'VDW', '0400'),
+                 ('water', 'VDW', '0250'),
                  ('water', 'VDW', '0500'),
-                 ('water', 'VDW', '0600'),
-                 ('water', 'VDW', '0650'),
-                 ('water', 'VDW', '0700'),
-                 ('water', 'VDW', '0750'),
-                 ('water', 'VDW', '0800'),
-                 ('water', 'VDW', '0850'),
-                 ('water', 'VDW', '0900'),
-                 ('water', 'VDW', '0950'),
                  ('water', 'VDW', '1000')]
 
 
@@ -58,44 +44,43 @@ class TestEnsemble(object):
 
     def test_build_ensemble(self):
         with in_dir(os.path.join(self.resources, 'states'), create=False):
-            bnz = Ensemble(dirname='benzene', solvents=['water'])
+            bnz = Ensemble(dirname='example_FEP', solvents=['water'])
             diff = set(bnz.get_keys()) ^ set(ensemble_keys)
             assert not diff
 
     def test_add_remove_systems(self):
         with in_dir(os.path.join(self.resources, 'states'), create=False):
             bnz = Ensemble()
-            l_dir = os.path.join(os.curdir, 'benzene', 'FEP', 'water', 'VDW', '0000')
+            l_dir = os.path.join(os.curdir, 'example_FEP', 'FEP', 'water', 'Coulomb', '0000')
             top_dir = os.path.join(l_dir, 'md.gro')
-            trj_dir = os.path.join(l_dir, 'md.xtc')
-            bnz.add_system(('water', 'VDW', '0000'), topology=top_dir, trajectory=trj_dir)
-            assert bnz.get_keys() == [('water', 'VDW', '0000')]
+            trj_dir = os.path.join(l_dir, 'md_red.xtc')
+            bnz.add_system(('water', 'Coulomb', '0000'), topology=top_dir, trajectory=trj_dir)
+            assert bnz.get_keys() == [('water', 'Coulomb', '0000')]
             assert bnz.num_systems == 1
             assert bnz.__repr__() == "<Ensemble Containing 1 System>"
             assert len(bnz) == 1
-            bnz.pop_system(('water', 'VDW', '0000'))
+            bnz.pop_system(('water', 'Coulomb', '0000'))
             assert bnz.num_systems == 0
             assert len(bnz) == 0
 
     def test_selection(self):
         with in_dir(os.path.join(self.resources, 'states'), create=False):
-            bnz = Ensemble(dirname='benzene', solvents=['water'])
+            bnz = Ensemble(dirname='example_FEP', solvents=['water'])
             solute = bnz.select_atoms('not resname SOL')
             for k in solute.group_keys():
-                assert len(solute[k]) == 12
+                assert len(solute[k]) == 42
 
     def test_ensemble_ag_methods(self):
         with in_dir(os.path.join(self.resources, 'states'), create=False):
-            BW = Ensemble(dirname='benzene', solvents=['water'])
-            BO = Ensemble(dirname='benzene', solvents=['octanol'])
+            BW = Ensemble(dirname='example_FEP', solvents=['water'])
         Sol1 = BW.select_atoms('resname SOL')
-        Sol2 = BO.select_atoms('resname SOL')
-        Sol1_pos = Sol1.select_atoms('resid 1').positions()
-        for k in Sol1_pos:
-            assert np.shape(Sol1_pos[k]) == (3, 3)
+        Sol2 = Sol1.select_atoms('resid 1')
+        Sol2_pos = Sol2.positions()
+        for k in Sol2_pos:
+            assert np.shape(Sol2_pos[k]) == (3, 3)
         assert not Sol1 == Sol2
-        assert type(Sol1.select_atoms('resid 1')) == EnsembleAtomGroup
-        assert Sol1.select_atoms('resid 1') == Sol1.select_atoms('resid 1')
+        assert type(Sol2) == EnsembleAtomGroup
+        assert Sol2 == Sol1.select_atoms('resid 1')
         assert BW.get_keys() == Sol1.ensemble().get_keys()
 
     def test_ensemble_analysis(self):
@@ -112,12 +97,12 @@ class TestEnsemble(object):
                 self.key_list.append(self._key)
 
             def _single_frame(self):
-                assert len(self._system.select_atoms('not resname SOL')) == 12
+                assert len(self._system.select_atoms('not resname SOL')) == 42
 
             def _conclude_universe(self):
                 assert self.n_frames == self.stop
 
         with in_dir(os.path.join(self.resources, 'states'), create=False):
-            BNZ = Ensemble(dirname='benzene', solvents=['water'])
+            BNZ = Ensemble(dirname='example_FEP', solvents=['water'])
         TestRun = TestAnalysis(BNZ).run(start=0, step=1, stop=10)
         assert BNZ.get_keys() == TestRun.key_list
