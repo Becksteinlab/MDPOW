03330c4875f726165537b5ea10bce70c28ab4df8
ALescoulie <alescoul@calpoly.edu>
2021-09-10 11:43:29 -0700

    modify dihedral.py to accept list

--- mdpow/analysis/dihedral.py
+++ mdpow/analysis/dihedral.py
@@ -1,11 +1,12 @@
 # MDPOW: dihedral.py
 # 2021 Alia Lescoulie
 
+from typing import List
+
 import numpy as np
 import pandas as pd
 
 from MDAnalysis.analysis.dihedrals import calc_dihedrals
-
 
 from .ensemble import Ensemble, EnsembleAtomGroup, EnsembleAnalysis
 
@@ -20,22 +21,25 @@ class DihedralAnalysis(EnsembleAnalysis):
     with four atoms selected on each.
     """
 
-    def __init__(self, DihedralGroups: EnsembleAtomGroup):
-        super(DihedralAnalysis, self).__init__(DihedralGroups.ensemble())
-        self._sel = DihedralGroups
+    def __init__(self, dihedralgroups: List[EnsembleAtomGroup]):
+        super(DihedralAnalysis, self).__init__(dihedralgroups[0].ensemble())
+        self._sel = dihedralgroups
 
     def _prepare_ensemble(self):
-        self._col = ['solvent', 'interaction',
+        self._col = ['selection', 'solvent', 'interaction',
                      'lambda', 'time', 'dihedral']
         self.results = pd.DataFrame(columns=self._col)
         self._res_dict = {key: [] for key in self._col}
 
     def _single_frame(self):
-        angle = calc_dihedrals(self._sel[self._key].positions[0], self._sel[self._key].positions[1],
-                               self._sel[self._key].positions[2], self._sel[self._key].positions[3])
-        angle_data = [self._key[0], self._key[1], self._key[2], self._ts.time, angle]
-        for i in range(len(self._col)):
-            self._res_dict[self._col[i]].append(angle_data[i])
+        for group in self._sel:
+            angle = calc_dihedrals(group[self._key].positions[0], group[self._key].positions[1],
+                                   group[self._key].positions[2], group[self._key].positions[3])
+            name = ''.join([x.name for x in group[self._key].atoms])
+            angle_data = [name, self._key[0], self._key[1],
+                          self._key[2], self._ts.time, angle]
+            for i in range(len(self._col)):
+                self._res_dict[self._col[i]].append(angle_data[i])
 
     def _conclude_ensemble(self):
         for k in self._col:
--- mdpow/tests/test_dihedral.py
+++ mdpow/tests/test_dihedral.py
@@ -35,10 +35,11 @@ class TestDihedral(object):
 
     def test_dataframe(self):
         dh1 = self.Ens.select_atoms('name C4 or name C17 or name S2 or name N3')
-        dh_run = DihedralAnalysis(dh1).run(start=0, stop=4, step=1)
+        dh_run = DihedralAnalysis([dh1]).run(start=0, stop=4, step=1)
 
         results = dh_run.results
 
+        assert results['selection'][0] == 'S2N3C4C17'
         for s in results['solvent']:
             assert s == 'water'
         for i in results['interaction'][:12]:
