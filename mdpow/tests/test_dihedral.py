f7a4a55636d3fd3a08b49dd8e07f618fddd65639
ALescoulie <alescoul@calpoly.edu>
2021-09-11 15:51:31 -0700

    add tests

--- mdpow/tests/test_dihedral.py
+++ mdpow/tests/test_dihedral.py
@@ -10,7 +10,9 @@ import pybol
 import pytest
 
 import numpy as np
+from numpy.testing import assert_almost_equal
 import pandas as pd
+from scipy.stats import circvar, circmean
 
 from MDAnalysis.exceptions import SelectionError
 
@@ -24,6 +26,11 @@ MANIFEST = RESOURCES.join("manifest.yml")
 
 
 class TestDihedral(object):
+    DG48910_mean = -172.9849512527183
+    DG491011_mean = -3.7300197060478695
+    DG48910_var = 1490.6576365537262
+    DG491011_var = 128.3805265432388
+
     def setup(self):
         self.tmpdir = td.TempDir()
         self.m = pybol.Manifest(str(RESOURCES / 'manifest.yml'))
@@ -49,3 +56,32 @@ class TestDihedral(object):
         dh1 = self.Ens.select_atoms('name C17 or name S2 or name N3')
         with pytest.raises(SelectionError):
             dh_run = DihedralAnalysis([dh1]).run(start=0, stop=4, step=1)
+
+    def test_results_recursive1(self):
+        dh1 = self.Ens.select_atoms('name C11 or name C10 or name C9 or name C4')
+        dh2 = self.Ens.select_atoms('name C11 or name C10 or name C9 or name C4')
+
+        dh_run1 = DihedralAnalysis([dh1]).run(start=0, stop=4, step=1)
+        dh_run2 = DihedralAnalysis([dh2]).run(start=0, stop=4, step=1)
+        assert len(dh_run1.results['dihedral']) == len(dh_run2.results['dihedral'])
+        for i in range(len(dh_run1.results['dihedral'])):
+            assert dh_run1.results['dihedral'][i] == dh_run2.results['dihedral'][i]
+
+    def test_results_recursive2(self):
+        dh1 = self.Ens.select_atoms('name C11 or name C10 or name C9 or name C4')
+        dh2 = self.Ens.select_atoms('name C8 or name C4 or name C9 or name C10')
+
+        dh_run = DihedralAnalysis([dh1, dh2]).run(start=0, stop=4, step=1)
+
+        dh1_result = dh_run.results.loc[dh_run.results['selection'] == 'C4-C9-C10-C11']['dihedral']
+        dh2_result = dh_run.results.loc[dh_run.results['selection'] == 'C4-C8-C9-C10']['dihedral']
+
+        dh1_mean = circmean(dh1_result, high=180, low=-180)
+        dh2_mean = circmean(dh2_result, high=180, low=-180)
+        dh1_var = circvar(dh1_result, high=180, low=-180)
+        dh2_var = circvar(dh2_result, high=180, low=-180)
+
+        assert_almost_equal(self.DG48910_mean, dh1_mean, 6)
+        assert_almost_equal(self.DG48910_var, dh1_var, 6)
+        assert_almost_equal(self.DG491011_mean, dh2_mean, 6)
+        assert_almost_equal(self.DG491011_var, dh2_var, 6)
