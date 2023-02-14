28c72ae8f93ccc9b73589e035f0a66abbca4acf0
cadeduckworth <crduckwo@asu.edu>
2023-02-14 01:56:02 -0700

    fix import names

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -2,7 +2,7 @@
 # 2023 Cade Duckworth
 
 # import analysis
-from mdpow.workflows import dihedrals_217 as dihedrals
+from mdpow.workflows import dihedrals
  
 registry = {
     'DihedralAnalysis' : dihedrals.automated_dihedral_analysis,
