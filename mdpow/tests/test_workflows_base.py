f6dbf55b2283ba8b13ad8a2b86064a5e24044388
cadeduckworth <crduckwo@asu.edu>
2023-02-14 02:28:38 -0700

    initialize workflows tests

--- mdpow/tests/test_workflows_base.py
+++ mdpow/tests/test_workflows_base.py
@@ -1,11 +1,10 @@
-'''from __future__ import absolute_import
-# review and remove remaning unnecessary code 
 import os
 import sys
 import yaml
 import pybol
 import pytest
 import pathlib
+import logging
 
 import scipy
 import numpy as np
@@ -13,6 +12,8 @@ import pandas as pd
 
 import rdkit
 from rdkit import Chem
+
+import seaborn
 
 from numpy.testing import assert_almost_equal
 from scipy.stats import circvar, circmean
@@ -31,7 +32,7 @@ MANIFEST = RESOURCES / "manifest.yml"
 
 @pytest.fixture(scope="function")
 def molname_workflows_directory(tmp_path, molname='SM25'):
-    m = pybol.Manifest(str(RESOURCES / 'manifest.yml'))
+    m = pybol.Manifest(str(MANIFEST))
     m.assemble('workflows', tmp_path)
     return tmp_path / molname
 
@@ -42,19 +43,6 @@ class TestAutomatedDihedralAnalysis(object):
         dirname = molname_workflows_directory
         return dirname
 
-    @pytest.fixture(scope="function")
-    def gen_data(self, SM25_tmp_dir):
-        atom_group_indices = dihedrals.dihedral_indices(dirname=SM25_tmp_dir, resname=self.resname)
-        # test user input of SMARTS string
-        atom_group_indices_alt = dihedrals.dihedral_indices(dirname=SM25_tmp_dir, resname=self.resname,
-                                                            SMARTS='[!$(*#*)&!D1]-!@[!$(*#*)&!D1]')
-        df = dihedrals.dihedral_groups_ensemble(atom_group_indices=atom_group_indices, dirname=SM25_tmp_dir,
-                                                solvents=('water',))
-        df_aug = dihedrals.periodic_angle(df)
-        return atom_group_indices, atom_group_indices_alt, df, df_aug
-        # gen_data[0]=atom_group_indices, gen_data[1]=atom_group_indices_alt,
-        # gen_data[2]=df, gen_data[3]=df_aug
-
     resname = 'UNK'
         
     def test_base(self, molname_workflows_directory, SM25_tmp_dir):
@@ -62,18 +50,18 @@ class TestAutomatedDihedralAnalysis(object):
         parent_directory = molname_workflows_directory
         directory_paths = base.directory_paths(parent_directory=parent_directory)
         assert (directory_paths['molecule'] == 'SM25').any() == True
+        assert (directory_paths['molecule'] == 'SM26').any() == True
         
     def test_directory_iteration(self, molname_workflows_directory, SM25_tmp_dir):
         parent_directory = molname_workflows_directory
         directory_paths = base.directory_paths(parent_directory=parent_directory)
-        directory_paths = directory_paths
+
         # change resname to match topology (every SAMPL7 resname is 'UNK')
         # only necessary for this dataset, not necessary for normal use
-        for f in directory_paths:
-            directory_paths.resname = 'UNK'
+        directory_paths['resname'] = 'UNK'
+
         base.directory_iteration(directory_paths, df_save_dir=SM25_tmp_dir, solvents=('water',),
-                                 ensemble_analysis='DihedralAnalysis', padding=45, width=0.9,
-                                 SMARTS='[!#1]~[!$(*#*)&!D1]-!@[!$(*#*)&!D1]~[!#1]')
-        assert SM25_tmp_dir / 'SM25' / 'SM25_full_df.bz2'
-        assert SM25_tmp_dir / 'SM26' / 'SM26_full_df.bz2'
-        '''
+                                 ensemble_analysis='DihedralAnalysis')
+
+        assert SM25_tmp_dir / 'SM25' / 'SM25_full_df.csv.bz2'
+        assert SM25_tmp_dir / 'SM26' / 'SM26_full_df.csv.bz2'
