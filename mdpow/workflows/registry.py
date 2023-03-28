9c8334bf4b4354d2616fac36d77b64bcc4e57d84
cadeduckworth <crduckwo@asu.edu>
2023-03-28 03:30:18 -0700

    doc changes for registry

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -5,15 +5,17 @@
 :mod:`mdpow.workflows.registry` --- Registry of currently supported automated workflows
 =======================================================================================
 
-Each entry in :mod:`mdpow.workflows.registry` corresponds to an :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` for which exists a corresponding
-automated workflow.
+Each entry in :mod:`mdpow.workflows.registry` corresponds to an :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`
+for which exists a corresponding automated workflow.
+Currently supported workflows: :class:~`mdpow.analysis.dihedral.DihedralAnalysis`
+
+.. data:: registry
 
 Intended for use with :mod:`mdpow.workflows.base` to specify which
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should be iterated over
 the provided project data.
 
-.. seealso:: :mod:`~mdpow.workflows.base`
-.. seealso:: :mod:`~mdpow.workflows.dihedrals`
+.. seealso:: :mod:`~mdpow.workflows.base`, :mod:`~mdpow.workflows.dihedrals`
 
 """
 
@@ -21,9 +23,7 @@ the provided project data.
 from mdpow.workflows import dihedrals
  
 registry = {
-    'DihedralAnalysis' : dihedrals.automated_dihedral_analysis,
-    'SolvationAnalysis' : None,
-    'HBondAnalysis' : None
+    'DihedralAnalysis' : dihedrals.automated_dihedral_analysis
 }
 
 
