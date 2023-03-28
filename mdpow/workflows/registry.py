d1746fd8f35bf4d410f4c7749472a3f68d91af4f
cadeduckworth <crduckwo@asu.edu>
2023-03-28 04:42:51 -0700

    docs

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -8,9 +8,12 @@
 Each entry in :mod:`mdpow.workflows.registry` corresponds to an :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`
 for which exists a corresponding automated workflow.
 
-Currently supported workflows :mod:`~mdpow.workflows.dihedrals`
+Currently supported workflows - :mod:`~mdpow.workflows.dihedrals`
 
-.. data:: keys
+.. data:: `~mdpow.workflows.registry.registry`
+    To include a new automated workflow for use with :mod:`mdpow.workflows.base`,
+    create a key that is the name of the corresponding :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`,
+    with the value defined as `<module name>.<top-level function>`.
 
 Intended for use with :mod:`mdpow.workflows.base` to specify which
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should run iteratively over
@@ -26,8 +29,6 @@ from mdpow.workflows import dihedrals
 registry = {
     'DihedralAnalysis' : dihedrals.automated_dihedral_analysis
 }
-
-keys = registry.keys()
 
     
     
