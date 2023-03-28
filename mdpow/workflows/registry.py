cba55136e0de296fd86b9a313c3159f3331545c9
cadeduckworth <crduckwo@asu.edu>
2023-03-28 03:52:19 -0700

    registry docs

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -10,10 +10,12 @@ for which exists a corresponding automated workflow.
 
 Currently supported workflows :class:`~mdpow.analysis.dihedral.DihedralAnalysis`
 
-.. data:: registry.registry
+.. obj:: `~mdpow.workflows.registry.registry`
+
+    :data:
 
 Intended for use with :mod:`mdpow.workflows.base` to specify which
-:class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should be iterated over
+:class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should run iteratively over
 the provided project data.
 
 .. seealso:: :mod:`~mdpow.workflows.base`, :mod:`~mdpow.workflows.dihedrals`
