f26dfb4ba27d23373ee96e957e2ad45f4a9d412d
cadeduckworth <crduckwo@asu.edu>
2023-03-28 05:09:44 -0700

    docs

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -5,7 +5,7 @@
 :mod:`mdpow.workflows.registry` --- Registry of currently supported automated workflows
 =======================================================================================
 
-.. autodata:: registry
+.. data:: registry
 
 Each entry in :mod:`mdpow.workflows.registry` corresponds to an :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`
 for which exists a corresponding automated workflow.
@@ -17,7 +17,7 @@ Intended for use with :mod:`mdpow.workflows.base` to specify which
 the provided project data directory. To include a new automated workflow for use with
 :mod:`mdpow.workflows.base`, create a key that is the name of the corresponding
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`, with the value defined as
-`<module name>.<top-level function>`.
+`<workflow module name>.<top-level function>`.
 
 .. seealso:: :mod:`~mdpow.workflows.base`, :mod:`~mdpow.workflows.dihedrals`
 
