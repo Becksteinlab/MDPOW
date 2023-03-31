3f085d17a5ce9832d7295157bc9b6f270135db71
cadeduckworth <crduckwo@asu.edu>
2023-03-31 16:31:36 -0700

    registry docs

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -18,13 +18,6 @@ for which exists a corresponding automated workflow.
    | DihedralAnalysis              | :any:`dihedrals.automated_dihedral_analysis <mdpow.workflows.dihedrals.automated_dihedral_analysis>` |
    +-------------------------------+------------------------------------------------------------------------------------------------------+
 
-Intended for use with :mod:`mdpow.workflows.base` to specify which
-:class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should run iteratively over
-the provided project data directory. To include a new automated workflow for use with
-:mod:`mdpow.workflows.base`, create a key that is the name of the corresponding
-:class:`~mdpow.analysis.ensemble.EnsembleAnalysis`, with the value defined as
-`<workflow module>.<top-level automated analysis function>`.
-
 .. autodata:: registry
 
 .. seealso:: :mod:`~mdpow.workflows.base`
@@ -33,7 +26,20 @@ the provided project data directory. To include a new automated workflow for use
 
 # import analysis
 from mdpow.workflows import dihedrals
- 
+
+"""
+.. :data: mdpow.workflows.registry.registry
+
+   Intended for use with :mod:`mdpow.workflows.base` to specify which
+   :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should run iteratively over
+   the provided project data directory. To include a new automated workflow for use with
+   :mod:`mdpow.workflows.base`, create a key that is the name of the corresponding
+   :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`, with the value defined as
+   `<workflow module>.<top-level automated analysis function>`.
+
+   The currently available workflows are listed in the :ref:`Currently supported automated workflows. <mdpow.workflows.registry.workflows_registry>`
+
+"""
 registry = {
     'DihedralAnalysis' : dihedrals.automated_dihedral_analysis
 }
