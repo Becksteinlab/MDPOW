e337d2909efcb6271dab32cdabf571084f01673d
cadeduckworth <crduckwo@asu.edu>
2023-03-31 17:56:09 -0700

    registry docs

--- mdpow/workflows/registry.py
+++ mdpow/workflows/registry.py
@@ -5,7 +5,7 @@
 :mod:`mdpow.workflows.registry` --- Registry of currently supported automated workflows
 =======================================================================================
 
-:mod:`mdpow.workflows.registry` hosts a dictionary with keys that correspond to an
+The :mod:`mdpow.workflows.registry` module hosts a dictionary with keys that correspond to an
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` for which exists a corresponding automated workflow.
 
 .. table:: Currently supported automated workflows.
@@ -34,17 +34,20 @@ registry = {
 }
 
 """
-Each entry corresponds to an :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`
+In the `registry`, each entry corresponds to an
+:class:`~mdpow.analysis.ensemble.EnsembleAnalysis`
 for which exists a corresponding automated workflow.
 
 Intended for use with :mod:`mdpow.workflows.base` to specify which
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis` should run iteratively over
-the provided project data directory. To include a new automated workflow for use with
-:mod:`mdpow.workflows.base`, create a key that is the name of the corresponding
+the provided project data directory.
+
+To include a new automated workflow for use with :mod:`mdpow.workflows.base`,
+create a key that is the name of the corresponding
 :class:`~mdpow.analysis.ensemble.EnsembleAnalysis`, with the value defined as
 `<workflow module>.<top-level automated analysis function>`.
 
-The currently available workflows are listed in the
-:any:`Currently supported automated workflows. <workflows_registry>`
+The available automated workflows (key-value pairs) are listed in the
+following table :any:`Currently supported automated workflows. <workflows_registry>`
 
 """
