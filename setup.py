23dfd1d3a06380ee28dd97d418fa322d434db1c1
Oliver Beckstein <orbeckst@gmail.com>
2024-10-09 18:08:20 -0700

    drop Python 3.8

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Chemistry",
